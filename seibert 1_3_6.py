36feed6b70553e413140546e241de85e05e695a4
Shannon Seibert <seiberts@swdubois.k12.in.us>
2020-02-03 14:14:29 -0500

    Create seibert 1_3_6.py

---
+++ seibert 1_3_6.py
@@ -0,0 +1,11 @@
+import random
+def roll_two_dice():
+    die1 = random.choice([1,2,3,4,5,6])
+    die2 = random.randint(1,6)
+    print( die1)
+    print ( die2)
+    return die1 + die2
+    
+def pick_letter():
+    return random.choice('seibert is awesome')
+    print ('test')
